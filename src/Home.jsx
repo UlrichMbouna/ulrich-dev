1f4e0c15a88b134fedad05bbc6dc3100e68021cb
ulrich <koudjouulrich@gmail.com>
2025-12-17 10:22:05 +0100

    redirect

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import './App.css';
 import { auto } from '@popperjs/core';
 import '@fortawesome/fontawesome-free/css/all.min.css';
